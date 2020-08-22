f57e8ee3250e7a183580ef40060d5fa94b8724a9
Yorker <yankees2484@gmail.com>
2020-08-22 10:43:35 +0800

    08/22

---
+++ public/EmbededContent/audio/audio.js
@@ -0,0 +1,65 @@
+﻿var musics = [
+  { name: "十年", url: "十年.mp3" },
+  { name: "K歌之王", url: "K歌之王.mp3" },
+  { name: "兄妹", url: "兄妹.mp3" },
+  { name: "好就不見", url: "好久不見.mp3" },
+  { name: "我們都寂寞", url: "我們都寂寞.mp3" }];
+var index = 0;
+//like & shuffle button
+$('.heart').click(function () {
+  $(this).toggleClass('clicked');
+});
+
+$('.shuffle').click(function () {
+  $(this).toggleClass('clicked');
+
+  var i = Math.floor(Math.random()*musics.length);  //取得隨機序
+  playsong(i);
+});
+
+//show info box on hover
+$('#player').hover(function () {
+  $('.info').toggleClass('up');
+});
+
+//music player settings
+
+var audio = new Audio();
+
+let timecurrent = $('.time--current');
+let timetotal = $('.time--total');
+let fill = $('.fill');
+let bar = $('.progress-bar');
+let fillTotal = bar.width();
+
+$('.pause').hide(); //hide pause button until clicked
+
+//play button
+$('.play').click(function () {
+  playsong(0);
+  $('.play').hide();
+  $('.pause').show();
+});
+
+//pause button
+$('.pause').click(function () {  
+  audio.pause();
+  $('.play').show();
+  $('.pause').hide();
+});
+
+function playsong(i){
+  index = i;
+  var playFile = musics[i];
+  audio.attributes("src","../music/"+playFile.url);
+  audio.play();
+  $("#song-name").text(playFile.name);
+}
+
+bar.click(function(e){
+  let s = audio.duration*(e.offsetX/fillTotal);
+  audio.currentTime = s;
+});
+
+audio.addEventListener("ended",next,false);
+ 
